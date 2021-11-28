31261c50b43eeb6e6bc9075855dc515a3086e5f5
Rendi Nusa <rendi.nusa@doterb.com>
2021-11-28 15:51:00 +0700

    Update hello with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/rendinusa/go-hello
+module github.com/rendinusa/go-hello-v2
 
 go 1.17
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello My Name Is Go!"
+func SayHello(name string) string {
+	return "Hello My Name Is " + name
 }
